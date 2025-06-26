c82275c026a234e80e09521b86f1dc487ead101d
vkp217 <vikaspvaletude@gmail.com>
2025-06-26 17:52:05 -0400

    some

--- src/ss/main.py
+++ src/ss/main.py
@@ -5,4 +5,5 @@ script_dir = os.path.abspath(os.getcwd())
 fname = os.path.abspath(os.path.join(script_dir, 'data', 'AF700_740bp_0001.hdf5'))
 
 tpsfs1, inten1, tpsfs2, inten2 = utils.ss3_read_hdf5_file(fname)    
+# Output modification code in the SwissSPAD data capture workflow
 output = utils.process_single_shot_lifetime(fname, gate=61, gate_width=3)
